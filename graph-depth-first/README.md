04663627a3959d899fa3ef4f3cf8a02f5fa32659
AntoineCharette <antoineocharette@gmail.com>
2021-12-10 23:17:59 -0500

    adds challenge 38

---
+++ graph-depth-first/README.md
@@ -0,0 +1,13 @@
+# Graph Depth First
+
+## Problem Domain
+
+In this code challenge, we are tasked with making a method of the graph class. Its parameter is a node and it should return a collection of nodes in the pre-ordered traversal order.
+
+## Algorithm
+
+The algorithm is basically the same as a tree depth first. You need to make sure to add to the visited nodes to make sure that the node isn't revisited.
+
+## Efficiency
+
+The time complexity is O(n) and space complexity is also O(n).
---
+++ graph-depth-first/graph-depth-first.js
@@ -0,0 +1,96 @@
+'use strict';
+
+class Vertex {
+  constructor(value) {
+    this.value = value;
+  }
+}
+
+class Edge {
+  constructor(vertex, weight) {
+    this.vertex = vertex;
+    this.weight = weight;
+  }
+}
+
+class Graph {
+  constructor() {
+    this.adjacencyList = new Map();
+    this.vertices = new Set();
+  }
+
+  addVertex(value) {
+    const payload = new Vertex(value);
+    this.adjacencyList.set(payload, []);
+    this.vertices.add(payload);
+    return payload;
+  }
+
+  addDirectEdge(startVertex, endVertex, weight = 0) {
+    if (!this.adjacencyList.has(startVertex) || !this.adjacencyList.has(endVertex)) {
+      throw new Error('Vertex Error');
+    }
+    const neighbors = this.adjacencyList.get(startVertex);
+    const newEdge = new Edge(endVertex, weight);
+    neighbors.push(newEdge);
+  }
+
+  getNeighbors(vertex) {
+    if (!this.adjacencyList.has(vertex)) {
+      throw new Error('getNeighbor error: invalid vertex');
+    }
+    return [...this.adjacencyList.get(vertex)];
+  }
+
+  breadthFirst(startVertex) {
+    const queue = [];
+    const visitedNodes = new Set();
+
+    queue.push(startVertex);
+    visitedNodes.add(startVertex);
+
+    while (queue.length) {
+      const current = queue.shift();
+      const neighbors = this.getNeighbors(current);
+
+      for (const edge of neighbors) {
+        const neighbor = edge.vertex;
+        if (!visitedNodes.has(neighbor)) {
+          queue.push(neighbor);
+          visitedNodes.add(neighbor);
+        } else {
+          continue;
+        }
+      }
+    }
+    return visitedNodes;
+  }
+
+  getNodes() {
+    const nodes = [];
+    this.adjacencyList.forEach((value, key) => {
+      nodes.push(key);
+    });
+    if (nodes.length === 0) return null;
+
+    return nodes;
+  }
+
+  size() {
+    return this.vertices.size;
+  }
+
+  depthFirst(vertex, visitedNodes = new Set()) {
+    visitedNodes.add(vertex);
+    const neighbors = this.getNeighbors(vertex);
+    for (const edge of neighbors) {
+      const neighbor = edge.vertex;
+      if (!visited.has(neighbor)) {
+        return this.depthFirst(neighbor, visitedNodes);
+      }
+    }
+    return visited;
+  }
+}
+
+module.exports = Graph;
